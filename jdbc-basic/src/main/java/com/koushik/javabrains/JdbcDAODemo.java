7e56989a00f11983773576690679069ac38ee1c7
Viraj Bhosle <bhosleviraj@gmail.com>
2018-10-08 15:13:24 +0530

    Using Hibernate with Spring

--- jdbc-basic/src/main/java/com/koushik/javabrains/JdbcDAODemo.java
+++ jdbc-basic/src/main/java/com/koushik/javabrains/JdbcDAODemo.java
@@ -3,13 +3,15 @@ package com.koushik.javabrains;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import com.koushik.javabrains.dao.HibernateDaoImpl;
 import com.koushik.javabrains.dao.SimpleJdbcDaoImpl;
 
 public class JdbcDAODemo {
 
 	public static void main(String[] args) {
 		ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
-		SimpleJdbcDaoImpl dao = context.getBean("simpleJdbcDaoImpl", SimpleJdbcDaoImpl.class);
+//		SimpleJdbcDaoImpl dao = context.getBean("simpleJdbcDaoImpl", SimpleJdbcDaoImpl.class);
+		HibernateDaoImpl dao = context.getBean("hibernateDaoImpl", HibernateDaoImpl.class);
 		System.out.println(dao.getCircleCount());
 	}
 
--- jdbc-basic/src/main/java/com/koushik/javabrains/model/Circle.java
+++ jdbc-basic/src/main/java/com/koushik/javabrains/model/Circle.java
@@ -1,7 +1,12 @@
 package com.koushik.javabrains.model;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
 public class Circle {
 
+	@Id
 	private int id;
 	private String name;
 	
